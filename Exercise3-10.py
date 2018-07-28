9eb04507a7ba30567b77c760b49e97613441a157
christianspiro <christianspiro1@gmail.com>
2018-07-28 14:04:24 -0400

    Chapter 3 Complete

---
+++ Exercise3-10.py
@@ -0,0 +1,15 @@
+languageList = ["english", "albanian", "japanese", "french", "italian"]
+# Languages not capitalized due to sort return non-alpha order
+print("I can currently speak " + languageList[0].title() + " and " + languageList[1].title() + ".")
+# Some functions can be used like JavaScript
+print("I will take " + languageList[2].title() + " at University.")
+print("Then I would like to learn either " + languageList[3].title() + " or " + languageList[4].title() + "." )
+# Backtick strings would be nice like in JavaScript ex) `#${variable} of stuff`
+languageList.append("russian")
+# Also could be cool to learn
+del languageList[5]
+popped = languageList.pop(1)
+print(popped)
+languageList.remove("italian")
+languageList.sort()
+# Every other function used in Chapter 3 occurs in previous exercise.
---
+++ Exercise3-9.py
@@ -0,0 +1,5 @@
+guests = ["martin", "luke", "charles", "ethan", "hassan", "krishna","tamjid"]
+print("I am expecting "+str(len(guests))+" guests to come to dinner!")
+
+# Don't forget its different from javascript, // does not make comments and list.something() is not how to call
+# a function. Use something(list) instead!
