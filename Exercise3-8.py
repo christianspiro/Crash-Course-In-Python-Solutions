4d2af3e4eab433b252b747be2f6ea8e6350610d1
christianspiro <christianspiro1@gmail.com>
2018-07-28 10:42:20 -0400

    First exercise that I started working on.

---
+++ Exercise3-8.py
@@ -0,0 +1,9 @@
+locations = ["tokyo","victoria", "nyc", "berlin", "tirana"]
+print(locations)
+print(sorted(locations))
+print(sorted(locations,reverse=True))
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+locations.sort
